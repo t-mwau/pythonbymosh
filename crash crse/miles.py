0e8be3f62e692a61f2ae07f97d81342bf6819021
T007-tim <timmulwa123@gmai.com>
2026-07-16 14:25:33 +0300

    This is my step process using pycharm to sharpen and grow my experience with python

---
+++ crash crse/miles.py
@@ -0,0 +1,4 @@
+#Ask the user for the distance travelled in miles convert it to kilometers and display the result
+distance_miles =input("Distance travelled in miles:")
+distance_kilometers = float(distance_miles) * 1.60934
+print(distance_kilometers)
